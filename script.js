a7abac1c6a8824b368831dc089c4854dfdf04e55
Ivan <z5557064@ad.unsw.edu.au>
2025-01-24 18:41:25 +0200

    Adding styling to the buttons and basic layout

--- script.js
+++ script.js
@@ -28,13 +28,24 @@ function operate(number1, number2, operator) {
     }
 }
 
+createButtons();
+
 function createButtons() {
     const arr = ['7', '8', '9', '÷', '4', '5', '6', '×', '1', '2', '3', '-', '.', '0', '=', '+'];
-    const numberPad = document.getElementsByClassName('numberPad');
+    const numberPad = document.getElementById('numberPad');
     for (let i = 0; i < 16; i++) {
-        let button = document.createAttribute('button');
+        let button = document.createElement('button');
         numberPad.appendChild(button);
-        button.textContent(arr[i]);
+        button.textContent = arr[i];
     }
+}
+
+const buttons = numberPad.querySelectorAll('button');
+buttons.forEach(setButtons);
+
+function setButtons(button) {
+    button.setAttribute('style', 'border-color: black; background-color: white; margin: 5px; font-weight: bold; font-size: 20px;');
+    button.style.height = '75px';
+    button.style.width = `${450/4}px`;
 }
 
