cc8afa75df8e8566c6b1c962c1897862d502cb66
Ivan <z5557064@ad.unsw.edu.au>
2025-01-27 21:56:33 +0200

    Added regex for checking patterns for the expressions and fixed expressions that start with an operator

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let number2, operator;
+let number2, operator, currentResult;
 let number1 = 0;
-let display = '0';
+let display = '';
+const operatorRegex = /[+×÷-]/;
 
 function add(number1, number2) {
     return number1 + number2;
@@ -20,13 +21,13 @@ function divide(number1, number2) {
 
 function operate(number1, number2, operator) {
     if (operator === '+') {
-        add(number1, number2);
+        return add(number1, number2);
     } else if (operator === '-') {
-        subtract(number1, number2);
+        return subtract(number1, number2);
     } else if (operator === '×') {
-        multiply(number1, number2);
+        return multiply(number1, number2);
     } else if (operator === '÷') {
-        divide(number1, number2);
+        return divide(number1, number2);
     }
 }
 
@@ -55,18 +56,26 @@ function setButtons(button) {
 
 const displayHistory = document.getElementById('history');
 const answer = document.getElementById('answer');
-displayHistory.textContent = display;
+displayHistory.textContent = '0';
 answer.textContent = number1;
 
 // fills the history section of the display with the buttons' text that were clicked.
 function populateDisplay(e) {
     if (e.target.innerText === '=') {
-        operate(number1, number2, operator);
-    } else if (checkOperator(e.target.innerText)) {
-        //make another function that checks if there is enough input for operating
+        if (checkValidInput(display)) {
+            currentResult = operate(number1, number2, operator);
+            answer.textContent = currentResult;
+            display = '' + currentResult;
+        }
+    } else if (checkOperator(e.target.innerText) && checkValidInput(display)) {
+        currentResult = operate(number1, number2, operator);
+        answer.textContent = currentResult;
+        display = currentResult + e.target.innerText;
+        displayHistory.textContent = display;
+    } else {
+        display = display.concat(e.target.innerText);
+        displayHistory.textContent = display;
     }
-    display = display.concat(e.target.innerText);
-    displayHistory.textContent = display;
 }
 
 const clear = document.getElementById('clear');
@@ -84,11 +93,33 @@ deleteButton.addEventListener('click', deleteDisplay);
 function deleteDisplay() {
     display = display.substring(0, display.length - 1);
     if (display === '') {
-        display = '0';
+        displayHistory.textContent = '0';
+    } else {
+        displayHistory.textContent = display;
     }
-    displayHistory.textContent = display;
 }
 
 function checkOperator(character) {
-    return ((character === '+') || (character === '-') || (character === '×') || (character === '÷'));
+    return operatorRegex.test(character);
+}
+
+function checkValidInput(expression) {
+    if (expression.match(/\d+[+×÷-]\d+/)) {
+        const expressionArray = expression.split(operatorRegex);
+        console.log(expressionArray);
+        if (expressionArray.length === 2) {
+            number1 = parseFloat(expressionArray[0]);
+            operator = expression[expression.search(operatorRegex)];
+            number2 = parseFloat(expressionArray[1]);
+            return true;   
+        }
+    }
+
+    if (expression.match(/[+×÷-]\d+/)) {
+        operator = expression[0];
+        number2 = parseFloat(expression.substring(1));
+        return true;
+    }
+
+    return false;
 }
